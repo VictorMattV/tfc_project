ef709f894836a2bc474c0a40b7b4347426b74645
Victor <matheus0588@outlook.com>
2022-11-25 10:56:18 -0300

    req09

--- app/backend/src/middlewares/userValidate.ts
+++ app/backend/src/middlewares/userValidate.ts
@@ -1,12 +1,23 @@
 import { NextFunction, Request, Response } from 'express';
+import UserModel from '../database/models/UserModel';
+import IUser from '../interfaces/IUser';
 
 export default class loginValidation {
+  private model = UserModel;
+
   loginValid = async (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
       return res.status(400).json({ message: 'All fields must be filled' });
     }
+
+    const user = await this.model.findOne({ where: { email } }) as IUser;
+
+    if (!user) {
+      return res.status(401).json({ message: 'Incorrect email or password' });
+    }
+
     next();
   };
 }
