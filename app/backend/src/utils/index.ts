0909ea967fb441b4c06786918e78b7852a777a8a
Victor <matheus0588@outlook.com>
2022-11-30 20:26:03 -0300

    reqs 29-30 fix commit

--- app/backend/src/utils/index.ts
+++ app/backend/src/utils/index.ts
@@ -2,6 +2,6 @@ const points = `SUM(home_team_goals > away_team_goals) * 3 +
  SUM(home_team_goals = away_team_goals)`;
 
 const efficiency = `CONVERT((SUM(home_team_goals > away_team_goals) * 3 + 
-SUM(home_team_goals = away_team_goals)) / (COUNT(home_team) * 3) * 100, DECIMAL(10,2))`;
+SUM(home_team_goals = away_team_goals)) / (COUNT(home_team) * 3) * 100, DECIMAL(15,2))`;
 
 export { points, efficiency };
